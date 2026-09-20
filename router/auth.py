a9757a63b9a29a1c3ae7b9a7c75f8acb4e678d1f
Shivamcc <shivam.singh@culinarycompass.in>
2026-09-20 16:00:56 +0530

    profile api done

--- router/auth.py
+++ router/auth.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter,Depends
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from database import get_db
 from schemas import UserCreate,UserLogin
 from models import User
-from security import hash_password
+from security import hash_password,verify_password,create_access_token,get_current_user
 
 router = APIRouter(prefix = "/auth",tags=['Authentication'])
 
@@ -26,6 +27,40 @@ def register(user:UserCreate,db:Session=Depends(get_db)):
     return{
         "message":"ser registered successfully",
         "user_id":new_user.id
+    }
+
+
+@router.post('/login')
+def login(user:UserLogin,db:Session=Depends(get_db)):
+
+
+    db_user = db.execute(select (User).where(User.email == user.email)).scalar_one_or_none()
+
+    if not db_user:
+        return {
+            "error":"Invalid credentials"
+        }
+
+    if not verify_password(user.password,db_user.hash_password):
+        return{
+            "error":"Invalid credentials"
+        }
+
+    #generating tokens
+
+    token = create_access_token(db_user.id)
+
+    return {
+        "message":"Login successful",
+        "access_token":token
+    }
+
+@router.get("/me")
+def profile(current_user:User=Depends(get_current_user)):
+    return{
+        "id":current_user.id,
+        "email":current_user.email,
+        "name":current_user.name
     }
 
     
--- security.py
+++ security.py
@@ -1,7 +1,20 @@
 from passlib.context import CryptContext
+from jose import jwt,JWTError
+from fastapi import Depends,HTTPException
+from database import get_db
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from models import User
+from datetime import datetime,timezone,timedelta
+from fastapi.security import HTTPAuthorizationCredentials,HTTPBearer
 
+
+SECRET_KEY = "YOUR_SECRET_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE = 30
 
 pwd_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
+security = HTTPBearer()
 
 
 def hash_password(password):
@@ -10,3 +23,47 @@ def hash_password(password):
 
 def verify_password(plain_password,hashed_password):
     return pwd_context.verify(plain_password,hashed_password)
+
+
+def create_access_token(user_id:int):
+    expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRE)
+
+
+    data = {
+        "user_id":user_id,
+        "exp":expire
+    }
+
+    token = jwt.encode(data,SECRET_KEY,algorithm=ALGORITHM)
+
+    return token
+
+
+def verify_token(token:str,db:Session):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+
+        user_id = payload.get("user_id")
+        if not user_id:
+            return None
+
+        user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+
+        if not user:
+            return None
+
+        return user
+    except JWTError:
+        return None
+
+
+def get_current_user(credentials:HTTPAuthorizationCredentials = Depends(security),db:Session=Depends(get_db)):
+    token = credentials.credentials
+    user = verify_token(token,db)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail = "Invalid token"
+        )
+
+    return user
